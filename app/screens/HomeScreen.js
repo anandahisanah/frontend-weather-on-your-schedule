f3f50303942ce1b46928615b0b67e24a669163ec
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-07-03 13:14:03 +0800

    build: weather now and the list

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -14,6 +14,57 @@ function HomeScreen({ navigation, route }) {
 
     const todayWeathers = Array(6).fill(null);
 
+    // parse datetime
+    const [dateString, setDateString] = useState(null);
+    const utcOffset = 8 * 60; // UTC+8
+    const formattedDateTime = dayjs(dateString).add(utcOffset, 'minute');
+    const formattedTime = formattedDateTime.format('HH:mm');
+
+    const [forecastNow, setForecastNow] = useState({});
+
+    // find forecast now
+    const findForecastNow = () => {
+        axios
+            .get(`https://backend-weather-on-your-schedule-production.up.railway.app/forecast/now-by-city?user_username=${username}`)
+            .then(response => {
+                // console.log(response.data.data)
+                setForecastNow(response.data.data);
+
+                // console.log(response.data.data.datetime)
+                const dateString = response.data.data.datetime ? response.data.data.datetime.substring(0, 19) : null;
+                setDateString(dateString);
+                // console.log(dateString)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    useEffect(() => {
+        findForecastNow();
+    }, []);
+
+    // get forecasts
+    const [forecasts, setForecasts] = useState([]);
+
+    // get event
+    const getForecast = () => {
+        axios
+            .get(`https://backend-weather-on-your-schedule-production.up.railway.app/forecast/get?user_username=${username}`)
+            .then(response => {
+                console.log(response.data.data);
+                setForecasts(response.data.data);
+
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    useEffect(() => {
+        getForecast();
+    }, []);
+
     // get events
     const [events, setEvents] = useState([]);
 
@@ -22,7 +73,6 @@ function HomeScreen({ navigation, route }) {
         axios
             .get(`https://backend-weather-on-your-schedule-production.up.railway.app/events?userUsername=${username}`)
             .then(response => {
-                console.log(response.data.data);
                 setEvents(response.data.data);
 
             })
@@ -34,6 +84,17 @@ function HomeScreen({ navigation, route }) {
     useEffect(() => {
         getEvent();
     }, []);
+
+    const weatherBackgroundMapping = {
+        "Cerah": require("../assets/weather-background/0-cerah.jpg"),
+        "Cerah Berawan": require("../assets/weather-background/1-cerah-berawan.jpg"),
+        "Berawan": require("../assets/weather-background/3-berawan.jpg"),
+        "Berawan Tebal": require("../assets/weather-background/4-berawan-tebal.jpg"),
+        "Kabut": require("../assets/weather-background/45-kabut.jpg"),
+        "Hujan Ringan": require("../assets/weather-background/60-hujan-ringan.jpg"),
+        "Hujan Sedang": require("../assets/weather-background/61-hujan-sedang.jpg"),
+        "Hujan Petir": require("../assets/weather-background/95-hujan-petir.jpg"),
+    };
 
     const weatherImageMapping = {
         "Cerah": require("../assets/weather-icon/0-cerah.png"),
@@ -52,31 +113,31 @@ function HomeScreen({ navigation, route }) {
                 {/* top */}
                 <View style={tw`mb-3`}>
                     <ImageBackground
-                        source={require('../assets/white-cloud.jpg')}
+                        source={weatherBackgroundMapping[forecastNow.weather]}
                         style={tw`items-center py-5`}
                         imageStyle={{ opacity: 0.5 }}
                     >
                         <View style={tw`items-center`}>
-                            <Text style={tw`mb-2 font-medium text-base`}>12:00</Text>
-                            <Text style={tw`mb-2 font-medium text-lg`}>Balikpapan</Text>
-                            <Text style={tw`mb-2 font-medium text-6xl`}>30°</Text>
+                            <Text style={tw`mb-2 font-medium text-base`}>Today {formattedTime}</Text>
+                            <Text style={tw`mb-2 font-medium text-lg`}>{forecastNow.city_name}</Text>
+                            <Text style={tw`mb-2 font-medium text-6xl`}>{forecastNow.temperature}°</Text>
                             <View style={tw`flex-row items-center`}>
-                                <Image source={require('../assets/icon-sun.png')} style={tw`w-8 h-8 mx-2`} />
-                                <Text style={tw`mb-2 font-medium text-base`}>75%</Text>
+                                <Image source={weatherImageMapping[forecastNow.weather]} style={tw`w-8 h-8 mx-2`} />
+                                <Text style={tw`font-medium text-base`}>{forecastNow.weather}</Text>
                             </View>
                             <View style={tw`flex-row items-center`}>
-                                <Image source={require('../assets/icon-uv.png')} style={tw`w-8 h-8 mx-2`} />
-                                <Text style={tw`mb-2 font-medium text-base`}>High</Text>
+                                <Image source={require('../assets/icon-drip.png')} style={tw`w-8 h-8 mx-2`} />
+                                <Text style={tw`font-medium text-base`}>{forecastNow.humidity}%</Text>
                             </View>
                         </View>
                     </ImageBackground>
                 </View>
 
-                {/* today weather */}
+                {/* weather */}
                 <View style={tw`w-full px-5 mb-3`}>
                     <View style={tw`flex-row mb-3`}>
                         <View style={tw`flex-1`}>
-                            <Text style={tw`text-lg font-semibold`}>Today</Text>
+                            <Text style={tw`text-lg font-semibold`}>Weather</Text>
                         </View>
                         <TouchableOpacity
                             onPress={() => navigation.replace('Today', { username: username })}
@@ -85,17 +146,28 @@ function HomeScreen({ navigation, route }) {
                         </TouchableOpacity>
                     </View>
                     <View style={tw`flex-row`}>
-                        {todayWeathers.map((_, index) => (
-                            <View style={tw`flex-1 items-center mx-2`} key={index}>
-                                <Text style={tw`font-medium text-slate-500`}>14:00</Text>
-                                <Image source={require('../assets/icon-sun.png')} style={tw`w-12 h-12`} />
-                                <Text style={tw`font-medium text-base text-xl`}>30°</Text>
-                                <View style={tw`flex-row items-center`}>
-                                    <Image source={require('../assets/icon-drip.png')} style={tw`w-4 h-4 mx-2`} />
-                                    <Text style={tw`mb-2 font-medium`}>75%</Text>
+                        {forecasts.map((forecast, index) => {
+                            // parse date and time
+                            const dateString = forecast.datetime.substring(0, 19);
+                            const utcOffset = 8 * 60; // Offset waktu UTC+8 dalam menit (8 jam)
+                            const formattedDateTime = dayjs(dateString).add(utcOffset, 'minute');
+                            const formattedDate = formattedDateTime.format('DD MMM');
+                            const formattedTime = formattedDateTime.format('HH:mm');
+
+                            return (
+                                <View style={tw`flex-1 items-center mx-2`} key={index}>
+                                    <Text style={tw`font-medium text-slate-500`}>{formattedDate}</Text>
+                                    <Text style={tw`font-medium text-slate-500`}>{formattedTime}</Text>
+                                    <Image source={weatherImageMapping[forecast.weather]} style={tw`w-12 h-12`} />
+                                    <Text style={tw`font-medium text-base text-xl`}>{forecast.temperature}°</Text>
+                                    <View style={tw`flex-row items-center`}>
+                                        <Image source={require('../assets/icon-drip.png')} style={tw`w-4 h-4 mx-1`} />
+                                        <Text style={tw`font-medium`}>{forecast.humidity}%</Text>
+                                    </View>
                                 </View>
-                            </View>
-                        ))}
+                            );
+                        })}
+
                     </View>
                 </View>
 
